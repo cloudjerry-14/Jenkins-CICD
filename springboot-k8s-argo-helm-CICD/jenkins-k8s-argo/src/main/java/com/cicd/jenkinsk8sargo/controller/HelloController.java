e30cfe85053953a23b13cf8e9f6f1aa82e7ef4b3
csjerry <ashutoshparihar0@gmail.com>
2023-12-25 19:04:33 +0530

    locally working

--- springboot-k8s-argo-helm-CICD/jenkins-k8s-argo/src/main/java/com/cicd/jenkinsk8sargo/controller/HelloController.java
+++ springboot-k8s-argo-helm-CICD/jenkins-k8s-argo/src/main/java/com/cicd/jenkinsk8sargo/controller/HelloController.java
@@ -1,15 +1,13 @@
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@RestController
-public class HelloController{
-  
+package com.cicd.jenkinsk8sargo.controller;
 
-    @GetMapping("/")
-    public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a spring boot application using Maven");
-        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
-        return "index";
-    }
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
 }
