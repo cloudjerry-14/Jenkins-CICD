ca7d0f3f2e4fb313f7623816ab25ef0851f2e170
Ashutosh Parihar <71233890+cloudjerry-14@users.noreply.github.com>
2023-12-25 18:42:29 +0530

    Add files via upload

---
+++ springboot-k8s-argo-helm-CICD/jenkins-k8s-argo/src/main/java/com/cicd/jenkinsk8sargo/JenkinsK8sArgoApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.jenkinsk8sargo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsK8sArgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsK8sArgoApplication.class, args);
+	}
+
+}
---
+++ springboot-k8s-argo-helm-CICD/jenkins-k8s-argo/src/test/java/com/cicd/jenkinsk8sargo/JenkinsK8sArgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.jenkinsk8sargo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsK8sArgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
